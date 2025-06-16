e3a41dcea686a8390d1aed3fce83bfe4a7527832
lisa <lisawanjiku41@gmail.com>
2025-06-16 10:32:35 +0300

    array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+  // Find the first game where result is "W"
+  const win = record.find(game => game.result === "W");
+  
+  // If found, return the year; otherwise return undefined
+  if (win) {
+    return win.year;
+  } else {
+    return undefined;
+  }
+}
